43d0f13546934bc27649ca1cfd6abba60231502b
feichulin <feimosqueda777@gmail.com>
2026-04-12 22:07:00 -0300

    canbio a la structura del backend

---
+++ backend/src/public/index.php
@@ -0,0 +1,22 @@
+<?php
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+
+require __DIR__ . '/../../vendor/autoload.php';
+
+
+$app = AppFactory::create();
+
+
+$app->get('/', function (Request $request, Response $response, $args) {
+    $response->getBody()->write("Hello world!");
+    return $response;
+});
+
+
+
+// silencia el error de las devtools de google
+$errorMiddleware = $app->addErrorMiddleware(true, true, true);
+$app->run();
