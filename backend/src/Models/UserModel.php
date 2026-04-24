a5219dd7114834842a959ad9ec73a3a3830eba9e
feichulin <feimosqueda777@gmail.com>
2026-04-24 16:25:11 -0300

    añado al user model setokenNull, getByToken y updateToeken

--- backend/src/Models/UserModel.php
+++ backend/src/Models/UserModel.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Models;
 
 use PDO;
@@ -9,7 +10,47 @@ class UserModel
     {
         $stmt = $pdo->prepare("SELECT * FROM users WHERE email = :email");
         $stmt->execute(['email' => $email]);
- 
+
         return  $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+    public static function setTokenNull($pdo, $token)
+    {
+        $stmt = $pdo->prepare("
+        UPDATE users 
+        SET token = NULL, token_expired_at = NULL 
+        WHERE token = :token
+    ");
+
+        $stmt->execute([
+            "token" => $token
+        ]);
+    }
+    public static function getByToken($pdo, $token)
+    {
+        $stmt = $pdo->prepare("
+        SELECT id, token, token_expired_at 
+        FROM users 
+        WHERE token = :token
+    ");
+
+        $stmt->execute([
+            "token" => $token
+        ]);
+
+        return $stmt->fetch(\PDO::FETCH_ASSOC);
+    }
+    public static function updateToken($pdo, $id, $token, $expired)
+    {
+        $stmt = $pdo->prepare("
+        UPDATE users  
+        SET token = :token, token_expired_at = :expired  
+        WHERE id = :id
+    ");
+
+        $stmt->execute([
+            "token" => $token,
+            "expired" => $expired,
+            "id" => $id
+        ]);
     }
 };
