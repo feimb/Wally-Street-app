2b47340d39432872f4771f7546281a5dda05ee5b
feichulin <feimosqueda777@gmail.com>
2026-05-02 22:07:15 -0300

    añado el endpoint /users para crear usuarios
    use el codigo de leonardo

---
+++ backend/src/Controllers/UserController.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Controllers;
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use App\Models\UserModel;
+
+class UserController
+{
+    public function index(Request $request, Response $response): Response
+    {
+        $user_id = $request->getAttribute('usuario');
+
+        if (!UserModel::esAdmin($user_id)) {
+            $response->getBody()->write(json_encode([
+                "error" => "Acceso denegado"
+            ]));
+
+            return $response
+                ->withStatus(403)
+                ->withHeader('Content-Type', 'application/json');
+        }
+
+
+        $dato = UserModel::obtenerUsuarios();
+
+        if ($dato) {
+            $response->getBody()->write(json_encode([
+                $dato
+            ]));
+
+            return $response
+                ->withStatus(200)
+                ->withHeader('Content-Type', 'application/json');
+        }
+
+        $response->getBody()->write(json_encode([
+            "no hay usuario registrados",
+        ]));
+
+        return $response
+            ->withStatus(404)
+            ->withHeader('Content-Type', 'application/json');
+    }
+
+    public function retrieve(Request $request, Response $response): Response // crear usuario
+    {
+        $dato = json_decode($request->getBody()->getContents(), true) ?? [];
+
+        $email = trim(strtolower($dato['email'] ?? ''));
+        $password = $dato['password'] ?? '';
+        $nombre = $dato['nombre'] ?? '';
+
+        if (!$email || !$password || !$nombre) {
+            return $this->error($response, "faltan datos");
+        }
+
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            return $this->error($response, "email inválido");
+        }
+
+        if (strlen($password) < 8 || strlen($nombre) < 3) {
+            return $this->error($response, "datos muy cortos");
+        }
+
+        if ((bool)UserModel::existe($email)) {
+            return $this->error($response, "el usuario ya existe");
+        }
+        $passwordHash = password_hash($password, PASSWORD_BCRYPT);
+
+        UserModel::crearUsuario($email, $passwordHash, $nombre);
+        $response->getBody()->write(json_encode([
+            "mensaje" => "creado exitoso"
+        ]));
+
+        return $response
+            ->withHeader('Content-Type', 'application/json')
+            ->withStatus(201);
+    }
+
+    private function error($response, $mensaje)
+    {
+        $response->getBody()->write(json_encode([
+            "error" => $mensaje
+        ]));
+
+        return $response
+            ->withHeader('Content-Type', 'application/json')
+            ->withStatus(400);
+    }
+    public function ObtenerUsuario(Request $request, Response $response, $args): Response
+    {
+        $id = $args['id'];
+        $user_id = $request->getAttribute('usuario');
+
+        if (!UserModel::esAdmin($user_id)) {
+            $response->getBody()->write(json_encode([
+                "error" => "Acceso denegado"
+            ]));
+
+            return $response
+                ->withStatus(403)
+                ->withHeader('Content-Type', 'application/json');
+        }
+
+
+
+        $dato = UserModel::obtenerUsuario($id);
+
+        if ($dato) {
+            $response->getBody()->write(json_encode($dato));
+
+            return $response
+                ->withStatus(200)
+                ->withHeader('Content-Type', 'application/json');
+        }
+
+        $response->getBody()->write(json_encode([
+            "error" => "Usuario no encontrado"
+        ]));
+
+        return $response
+            ->withStatus(404)
+            ->withHeader('Content-Type', 'application/json');
+    }
+}
--- backend/src/Routes/web.php
+++ backend/src/Routes/web.php
@@ -6,6 +6,7 @@ use Slim\App;
 use App\app\Middleware\IsLoggedMiddleware;
 
 use App\Controllers\AuthController;
+use App\Controllers\UserController;
 
 return function (app $app) {
     // prueba de rutas protegidas con token
@@ -24,16 +25,23 @@ return function (app $app) {
         });
     })->add(new IsLoggedMiddleware($app->getResponseFactory()));
     // prueba de rutas protegidas con token
+    $app->get('/hola', function ($request, $response, $args) {
 
+        $usuario = $request->getAttribute('usuario');
+
+        $response->getBody()->write(json_encode([
+            "mensaje" => "Hello world!",
+            "usuario" => $usuario
+        ]));
+        return $response->withHeader('Content-Type', 'application/json');
+    });
     // Autenticacion
 
     $app->post('/login', [AuthController::class, 'login']);
     $app->post('/logout', [AuthController::class, 'logout']);
 
     // Usuarios
-    $app->get('/users', function ($request, $response, $args) {
-        return;
-    });
+    $app->post('/users', [UserController::class, 'retrieve']);
     $app->get('/users/{user_id}', function ($request, $response, $args) {
         return;
     });
