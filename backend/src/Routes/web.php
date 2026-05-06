dfbf442e1e9fc1c1f579c117020122e3662e363c
Leonardo Fernandez <leo.de24mg@gmail.com>
2026-05-06 14:08:27 -0300

    rutas de assets

--- backend/src/Routes/web.php
+++ backend/src/Routes/web.php
@@ -1,93 +1,14 @@
 
 <?php
+
 use Slim\App;
+use App\Controllers\AssetsController;
 use App\app\Middleware\IsLoggedMiddleware;
-use App\Controllers\AuthController;
-use App\Controllers\UserController;
-use App\Controllers\OperationsController;
+return function (App $app) {
 
-return function (app $app) {
-    // prueba de rutas protegidas con token
-    $app->group('/api', function ($group) {
-        $group->get('/', function ($request, $response, $args) {
-
-            $usuario = $request->getAttribute('usuario');
-
-            $response->getBody()->write(json_encode([
-                "mensaje" => "Hello world!",
-                "usuario" => $usuario
-            ]));
-
-
-            return $response->withHeader('Content-Type', 'application/json');
-        });
-        $group->put('/users/{user_id}', [UserController::class ,'updateUser']);
-        $group->get('/users/{user_id}', [UserController::class ,'ObtenerUsuario']);
-        $group->get('/users', [UserController::class ,'index']);
-
-
-    })->add(new IsLoggedMiddleware($app->getResponseFactory()));
-    // prueba de rutas protegidas con token
-    $app->get('/hola', function ($request, $response, $args) {
-
-        $usuario = $request->getAttribute('usuario');
-
-        $response->getBody()->write(json_encode([
-            "mensaje" => "Hello world!",
-            "usuario" => $usuario
-        ]));
-        return $response->withHeader('Content-Type', 'application/json');
-    });
-    // Autenticacion
-
-    $app->post('/login', [AuthController::class, 'login']);
-    $app->post('/logout', [AuthController::class, 'logout']);
-
-    // Usuarios
-    $app->post('/users', [UserController::class, 'retrieve']);
-    $app->get('/users/{user_id}', function ($request, $response, $args) {
-        return;
-    });
-    $app->post('/users/{user_id}', function ($request, $response, $args) {
-        return;
-    });
-
-    //Activos
-    $app->get('/assets', function ($request, $response, $args) {
-        // filtros
-        // ?type={name}
-        // min_price: Precio mínimo (ej: ?min_price=50).
-        // max_price: Precio máximo (ej: ?max_price=500).
-
-        return;
-    });
-    $app->get('/assets/{asset_id}/history/{quantity}', function ($request, $response, $args) {
-        return;
-    });
-    $app->put('/assets', function ($request, $response, $args) {
-        // <<< solo admin  >>>
-        // Dispara la actualización aleatoria de los precios de todos
-        // los activos
-
-        return;
-    });
-
-    // operaciones
-$app->post('/trade/sell', [OperationsController::class, 'sell'])->add(new IsLoggedMiddleware($app->getResponseFactory()));
-
-$app->post('/trade/buy', [OperationsController::class, 'buy'])->add(new IsLoggedMiddleware($app->getResponseFactory()));
-
-    // Portafolio y Historial
-    $app->get('/portfolio', function ($request, $response, $args) {
-        return;
-    });
-    $app->get('/transactions', function ($request, $response, $args) {
-        return;
-    });
-    $app->delete('/portfolio/{asset_id}', function ($request, $response, $args) {
-        return;
-    });
+$app->get('/assets', [AssetsController::class, 'index']);    // muestra los assets
+$app->put('/assets', [AssetsController::class, 'actualizarAssets'])->add(new IsLoggedMiddleware($app->getResponseFactory()));// actualiza los assets solo admin
+$app->get('/assets/{asset_id}/history/{quantity}', [AssetsController::class, 'retreive']); // muestra el historial de un assets
 
 
 };
-
