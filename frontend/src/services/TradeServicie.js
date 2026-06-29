858cf1ccefc47c8a453ceac15e8a6eec38943c88
Leonardo Fernandez <leo.de24mg@gmail.com>
2026-06-29 11:05:11 -0300

    error de merge con portfolio corregido

--- frontend/src/services/TradeServicie.js
+++ frontend/src/services/TradeServicie.js
@@ -1,12 +1,29 @@
 import axios from "axios";
 
-const API_URL = "http://localhost/trade/buy";
+const API_URL = "http://localhost/trade";
 
 export const BuyAsset = async (asset_id, quantity) => {
   const token = localStorage.getItem("token");
 
   const response = await axios.post(
-    API_URL,
+    `${API_URL}/buy`,
+    { asset_id, quantity },
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+
+  return response.data;
+};
+
+
+export const SellAsset = async (asset_id, quantity) => {
+  const token = localStorage.getItem("token");
+
+  const response = await axios.post(
+    `${API_URL}/sell`,
     { asset_id, quantity },
     {
       headers: {
