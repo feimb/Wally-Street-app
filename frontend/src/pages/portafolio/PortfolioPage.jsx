62d92486c50e5cddabd8bc8303e19661ed66d503
Leonardo Fernandez <leo.de24mg@gmail.com>
2026-06-29 10:30:52 -0300

    pagina de portaFolio

---
+++ frontend/src/pages/portafolio/PortfolioPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { PortfolioComponent } from "../../components/PortfolioComponent";
+
+export const Portfolio = () => {
+  return (
+    <div className="min-h-screen bg-black p-6">
+      <PortfolioComponent />
+    </div>
+  );
+};
+
+export default Portfolio;
