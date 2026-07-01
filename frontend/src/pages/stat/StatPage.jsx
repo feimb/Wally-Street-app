b430b95f955c50ddaabd63b328cbfba859b062c8
Leonardo Fernandez <leo.de24mg@gmail.com>
2026-07-01 11:00:39 -0300

    se actualizo la etique a Venta de Activos

--- frontend/src/pages/stat/StatPage.jsx
+++ frontend/src/pages/stat/StatPage.jsx
@@ -3,7 +3,7 @@ import PublicAssetsComponent from "../../components/PublicAssetsComponent";
 export default function StatPage() {
   return (
     <div>
-      <h1 className="text-2xl text-white font-bold text-center mt-6">Mercado</h1>
+      <h1 className="text-2xl text-white font-bold text-center mt-6">Mercado de Activos</h1>
       <PublicAssetsComponent />
     </div>
   );
